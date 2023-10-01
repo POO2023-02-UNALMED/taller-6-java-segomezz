996f900d697127f2a91afaa5a9180e070ebc709e
segomezz <141938389+segomezz@users.noreply.github.com>
2023-10-01 17:12:26 -0500

    Update Automovil.java

--- src/test/java/vehiculos/Automovil.java
+++ src/test/java/vehiculos/Automovil.java
@@ -23,16 +23,5 @@ public int getPuestos() {
 public void setPuestos(int puestos) {
 	this.puestos=puestos;
 }
-public static void main(String[] args) {
-	Pais p = new Pais("Colombia");
-	Fabricante f = new Fabricante("Renault", p);
-	Vehiculo sandero= new Vehiculo( "sa",4,60,"sachs",12000,39,"fwd",f);
-	Vehiculo.setCantidadVehiculos(0);
-	Camion c1 =  new Camion("SS", "Camion premium", 1000, 100, f, 3);
-	Camion c2 =  new Camion("AA", "Camion", 1000, 100, f, 3);
-	Automovil a1 =  new Automovil("NN", "Mac 100", 820, 20, f, 4);
-	Camioneta ca1 =  new Camioneta("CC", 5, "Ford", 500, 150, f, true);
-	System.out.println(Vehiculo.getCantidadVehiculos());
 
-}
 }
