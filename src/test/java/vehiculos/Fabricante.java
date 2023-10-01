a6915ba05a496a7b74aed76aade708fda0c67bf7
segomezz <141938389+segomezz@users.noreply.github.com>
2023-10-01 17:58:54 -0500

    Update Fabricante.java

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -3,7 +3,7 @@ package vehiculos;
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
-	Fabricante(String nombre,Pais pais){
+	public Fabricante(String nombre,Pais pais){
 		this.nombre=nombre;
 		this.pais=pais;
 	}
